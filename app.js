c880d1790fa66f4dde9cb72cb600af784aaa5ad6
Ranjit Sunil Somwanshi <ranjitdeshmukh1232@gmail.com>
2021-12-10 00:37:02 +0530

    comitted

---
+++ app.js
@@ -0,0 +1,28 @@
+const btnTranslate = document.querySelector("#btn-translate");
+const outputDiv = document.querySelector("#output");
+const textInput = document.querySelector("#text-input");
+
+const url = "https://api.funtranslations.com/translate/hodor.json";
+
+function urlcreator(text) {
+  return url + "?text=" + text;
+}
+
+function errorHandler(error) {
+  console.log("error occured", error);
+  alert("server down, try after some time");
+}
+
+btnTranslate.addEventListener("click", clickHandler);
+
+function clickHandler() {
+  var inputText = textInput.value;
+
+  fetch(urlcreator(inputText))
+    .then((response) => response.json())
+    .then((json) => {
+      var translatedText = json.contents.translated;
+      outputDiv.innerText = translatedText;
+    })
+    .catch(errorHandler);
+}
